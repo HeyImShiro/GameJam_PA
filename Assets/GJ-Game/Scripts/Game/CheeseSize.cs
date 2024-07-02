adbaea75be3b3120ddde5a7eb9b599629d1f3b02
Zati <Jonathan200@hotmail.de>
2024-07-02 18:50:12 +0200

    Cheese Size Downscale

--- Assets/GJ-Game/Scripts/Game/CheeseSize.cs
+++ Assets/GJ-Game/Scripts/Game/CheeseSize.cs
@@ -7,35 +7,52 @@ public class CheeseSize : MonoBehaviour
     public float minSize;
     public float maxSize;
 
-    private bool indicatorActive;
+    public float shrinkIntervall;
+    public float shrinkAmount;
+
+    [SerializeField] private bool indicatorActive;
+    private Material shader;
+
+    private float timer = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shader = GetComponentInChildren<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Shrink
+        timer += Time.deltaTime;
+        if(timer >= shrinkIntervall && transform.localScale.x >= minSize)
+        {
+            transform.localScale *= shrinkAmount;
+            timer = 0f;
+        }
+
+
+        //Activate Indicator when too small
         if(transform.localScale.x <= minSize + ((maxSize - minSize) * 0.2) && !indicatorActive)
         {
             //Activate Indicator Effect
-
+            shader.SetFloat("_BlinkActive", 1);
             indicatorActive = true;
         }
 
+        //Deactivate Indicator when not too small
         if(transform.localScale.x >= minSize + ((maxSize - minSize) * 0.2) && indicatorActive)
         {
             //Deactivate IndicatorEffect
-
+            shader.SetFloat("_BlinkActive", 0);
             indicatorActive = false;
         }
     }
 
     public void ChangeSize(float change)
     {
-        float changedSize = transform.localScale.x + ((maxSize - minSize) * (1 + change)) ;
+        float changedSize = transform.localScale.x + ((maxSize - minSize) * (1 + change));
         transform.localScale = new Vector3(changedSize, changedSize, changedSize);
     }
 }
