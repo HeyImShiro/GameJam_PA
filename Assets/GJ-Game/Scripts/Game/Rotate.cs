319f17693485bc788a5e9a2c06c47de5969db242
HeyImShiro <151179574+HeyImShiro@users.noreply.github.com>
2024-07-03 21:07:49 +0200

    windmill rotation
    windmühle dreht sich jetzt

--- Assets/GJ-Game/Scripts/Game/Rotate.cs
+++ Assets/GJ-Game/Scripts/Game/Rotate.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class Rotate : MonoBehaviour
 {
     public float rotationSpeed;
+    public bool rotateY;
 
     // Update is called once per frame
     void Update()
     {
         // Rotate
-        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+        if(rotateY)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+        }
+        else
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * rotationSpeed);
+        }
     }
 }
